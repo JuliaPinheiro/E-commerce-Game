2fcebe4404ea21a07a241de83494af86528891d1
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-12-04 23:54:39 -0300

    Update ProdutoRepository.java

--- src/main/java/com/julia/ecommercegame/repository/ProdutoRepository.java
+++ src/main/java/com/julia/ecommercegame/repository/ProdutoRepository.java
@@ -2,6 +2,12 @@ package com.julia.ecommercegame.repository;
 
 import com.julia.ecommercegame.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByNome(String nome);
+    List<Produto> findByPreco(Double preco);
 }
